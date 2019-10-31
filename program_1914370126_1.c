df012327c11e3c7914542f37257bc47c6487a96b
Muhammad Rizki <55613831+Rizki1914370126@users.noreply.github.com>
2019-11-01 00:02:36 +0700

    Add files via upload

---
+++ program_1914370126_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char NPM [30];
+    char Nama [30];
+    char Status;
+    int Nilai_UTS,Nilai_UAS;
+    float Nilai_Akhir;
+    if (Nilai_UTS > 70);
+    if (Nilai_UTS < 70);
+    Nilai_UTS = 75;
+    Nilai_UAS = 80;
+    Nilai_Akhir = (Nilai_UTS*60/100) + (Nilai_UAS*65/10)
+
+    ;printf("\nNama:Muhammad Rizki")
+    ;printf("\nNPM : 1914370126");
+    printf("\nNilai UTS \t : %i",Nilai_UTS);
+    printf("\nNILAI UAS \t : %i",Nilai_UAS);
+    printf("\nNilai AKHIR \t : %.2f", Nilai_Akhir);
+    printf("\nStatus = LULUS");
+    return 0;
+}
